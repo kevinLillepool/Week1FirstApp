3fdad19af0790baac8336a95b6a81661674eb1f7
Kevin Lillepool <kevin.lillepool@tktk.ee>
2024-10-24 21:51:37 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "kevin";
+string myLastName = "Lillepool";
+
+//Console.WriteLine("Hello" + " " + myname);
+
+Console.WriteLine($"Hello,{myName} {myLastName}!");
